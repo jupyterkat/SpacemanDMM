23b27995312246d2ca4fbece91d5b55956fe599f
Katherine Kiefer <sterlite2902@gmail.com>
2024-05-27 17:40:20 +1000

    hehe

--- crates/dmm-tools/src/lib.rs
+++ crates/dmm-tools/src/lib.rs
@@ -1,9 +1,6 @@
 //! SS13 minimap generation tool
 #![deny(unsafe_code)] // NB deny rather than forbid, ndarray macros use unsafe
 
-#[macro_use]
-extern crate bytemuck;
-
 #[cfg(feature = "gfx_core")]
 extern crate gfx_core;
 
